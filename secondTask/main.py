1d39593c3505f433e45d8fad490b5b74b1ae51df
ChloeKonde <konde.chloe@gmail.com>
2021-12-21 02:32:06 +0500

    minor fix

--- secondTask/main.py
+++ secondTask/main.py
@@ -1,53 +1,48 @@
-import os
 from twilio.rest import Client
 
 
-account_sid = 'ACca5a7f6c10411889563d7b39af291704'
-auth_token = '9c80ca0a7df2d5ec0872fd816a026d9b'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
-"""creating domain"""
-# domain = client.sip.domains.create(domain_name='astfgl.sip.twilio.com')
-# print(domain.sid)  # "SD19edea24ea258aaeabd98f6f17381a19" result
+domain = client.sip.domains.create(domain_name='astfgl.sip.twilio.com')
+print(domain.sid)  # "SD19edea24ea258aaeabd98f6f17381a19"
 
 
-"""fetching my domain from twilio"""
-sid = "SD19edea24ea258aaeabd98f6f17381a19"
-# domain = client.sip.domains(sid).fetch()
-# print(domain.domain_name)
+sid = 'SD19edea24ea258aaeabd98f6f17381a19'
+domain = client.sip.domains(sid).fetch()
+print(domain.domain_name)
 
-"""creating cred list"""
-# credential_list = client.sip.credential_lists.create(
-#                                                   friendly_name='credential_list'
-#                                               )
+credential_list = client.sip.credential_lists.create(
+                                                  friendly_name='credential_list'
+                                              )
 
-# print(credential_list.sid) # CLc69d93a2561f77d9b30bba6b7579ca3d
+print(credential_list.sid) # CLc69d93a2561f77d9b30bba6b7579ca3d
 
-"""fetching cred list from twilio"""
 
 cred_sid = "CLc69d93a2561f77d9b30bba6b7579ca3d"
-# credential_list = client.sip \
-#     .credential_lists(cred_sid) \
-#     .fetch()
+credential_list = client.sip \
+    .credential_lists(cred_sid) \
+    .fetch()
 
-# print(credential_list.friendly_name)
+print(credential_list.friendly_name)
 
-"""mappimg sid with creds"""
-# auth_calls_credential_list_mapping = client.sip \
-#     .domains(sid) \
-#     .auth \
-#     .calls \
-#     .credential_list_mappings \
-#     .create(credential_list_sid=cred_sid)
 
-# print(auth_calls_credential_list_mapping.sid) #CLc69d93a2561f77d9b30bba6b7579ca3d
+auth_calls_credential_list_mapping = client.sip \
+    .domains(sid) \
+    .auth \
+    .calls \
+    .credential_list_mappings \
+    .create(credential_list_sid=cred_sid)
 
-"""adding creds to cred list"""
-# credential = client.sip.credential_lists(cred_sid) \
-#                        .credentials \
-#                        .create(username='login', password='passwordPA33')
+print(auth_calls_credential_list_mapping.sid) #CLc69d93a2561f77d9b30bba6b7579ca3d
 
-# print(credential.sid) #CR5fe906ec4300c41fc34e2f2224c5bc96
+
+credential = client.sip.credential_lists(cred_sid) \
+                       .credentials \
+                       .create(username='login', password='passwordPA33')
+
+print(credential.sid) #CR5fe906ec4300c41fc34e2f2224c5bc96
 
 
 call = client.calls \
